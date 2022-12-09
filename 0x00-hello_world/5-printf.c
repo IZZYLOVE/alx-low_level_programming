5c4ede3ca5ebf627fd0835d77137370fc395873c
IZZYLOVE <izzylovu@gmail.com>
2022-12-08 21:35:23 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 /**
- * main - Entry point
- *
- * Description: 'a C program that prins a text, followed by a new line.'
- *
- * Return Always 0 (Success)
+ * main - Entry
+ * Return Always 0
  */
+main(void)
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
