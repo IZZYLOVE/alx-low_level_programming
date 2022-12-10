e94bff4e16bdcbe6fa3b017b637af262b3214343
IZZYLOVE <izzylovu@gmail.com>
2022-12-09 23:38:26 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
  * main - Entry
- * Return Always 0
+ * Return: Always 0
  */
-main(void)
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
