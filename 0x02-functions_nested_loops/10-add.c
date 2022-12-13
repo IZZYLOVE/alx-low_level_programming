a00e02c97a68337fb986f884ef8a35575d66cbef
IZZYLOVE <izzylovu@gmail.com>
2022-12-13 09:04:07 -0800

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ * @a: fist number
+ * @b: second number
+ * Return: Always (Sum) (success)
+ */
+int add(int a, int b)
+{
+	int Sum
+	Sum = a + b;
+	return (sum);
+}
