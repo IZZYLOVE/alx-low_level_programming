ad8f4d4acc4853209ab797a4a009dff1f6348b67
IZZYLOVE <izzylovu@gmail.com>
2022-12-13 13:18:11 -0800

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,25 +8,38 @@
 
 void print_times_table(int n)
 {
-	int num, mult, prod;
+	int a, b, op;
 
 	if (n >= 0 && n <= 15)
 	{
-		for (num = 0; num <= n; num++)
+		for (a = 0; a <= n; a++)
 		{
-			_putchar('0');
-			for (mult = 1; mult <= n; mult++)
+			_putchar(48);
+			for (b = 1; b <= n; b++)
 			{
-				_putchar(',');
-				_putchar(' ');
-				prod = num * mult;
-				if (prod <= n)
-					_putchar(' ');
+				op = a * b;
+				_putchar(44);
+				_putchar(32);
+				if (op <= 9)
+				{
+					_putchar(32);
+					_putchar(32);
+					_putchar(op + 48);
+				}
+				else if (op <= 99)
+				{
+					_putchar(32);
+					_putchar((op / 10) + 48);
+				}
 				else
-					_putchar((prod / 10) + '0');
-				_putchar((prod % 10) + '0');
+				{
+					_putchar(((op / 100) % 10) + 48);
+					_putchar(((op / 10) % 10) + 48);
+					_putchar((op % 10) + 48);
+				}
 			}
 			_putchar('\n');
+
 		}
 	}
 }
