01e2a26102df599671ce322715e7d183b2f74467
IZZYLOVE <izzylovu@gmail.com>
2022-12-15 22:06:49 +0100

    ./101-print_numbers

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - prints a Fizz Buzz number 1 to 100
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+	int n = 100;
+
+	if (n > 0)
+	{
+		for (i = 1; i <= n; i++)
+		{
+			if ((i % 3) == 0)
+				printf("Fizz");
+			if ((i % 5) == 0)
+				printf("Buzz");
+			if (!(((i % 3) == 0) || (i % 5) == 0))
+				printf("%d", i);
+			printf(" ");
+		}
+		printf("\n");
+	}
+	else
+	{
+		printf("\n");
+	}
+	return (0);
+}
