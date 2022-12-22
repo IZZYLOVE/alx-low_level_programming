83361b01cb149b51a93cc937f711c37201ed48da
IZZYLOVE <izzylovu@gmail.com>
2022-12-22 07:17:19 +0100

    100-rot13.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,41 +1,31 @@
 #include "main.h"
 
 /**
- * *cap_string - capitalizes all words of a string.
- *
- * @str: hold character.
- * Return: string.
+ * cap_string - capitalizes all words in a string
+ * @s: string
+ * Return: address of s
  */
-char *cap_string(char *str)
+char *cap_string(char *s)
 {
-	int i;
+	int i = 0, j;
+	char a[] = " \t\n,;.!?\"(){}";
 
-	for (i = 0; str[i] != '\0'; i++)
+	while (*(s + i))
 	{
-		if (i == 0)
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
 		{
-			if ((str[i] >= 97 && str[i] <= 122))
+			if (i == 0)
+				*(s + i) -= 'a' - 'A';
+			else
 			{
-				str[i] = str[i] - 32;
-				continue;
+				for (j = 0; j <= 12; j++)
+				{
+					if (a[j] == *(s + i - 1))
+						*(s + i) -= 'a' - 'A';
+				}
 			}
 		}
-		else if (str[i] == ' ')
-		{
-			++i;
-			if (str[i] >= 97 && str[i] <= 122)
-			{
-				str[i] = str[i] - 32;
-				continue;
-			}
-		}
-		else
-		{
-			if (str[i] >= 65 && str[i] <= 90)
-			{
-				str[i] = str[i] + 32;
-			}
-		}
+		i++;
 	}
-	return (str);
+	return (s);
 }
