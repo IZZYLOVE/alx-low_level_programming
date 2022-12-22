7f27a6bb5e9e9ef92883d801ae29e2d07325251c
IZZYLOVE <izzylovu@gmail.com>
2022-12-22 08:11:26 +0100

    101-print_number.c git push

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,18 +1,25 @@
 #include "main.h"
 
 /**
- * print_number - prints an interger.
- * @n: interger.
- * Return: nothing.
- */
+  * print_number - Prints any integer with putchar
+  * @n: Number to prints
+  *
+  * Return: Nothing
+  */
 void print_number(int n)
 {
+	unsigned int x;
+
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		n *= -1;
 	}
-	if (n / 10)
-		print_number(n / 10);
-	_putchar('0' + n % 10);
+
+	x = n;
+
+	if (x / 10)
+		print_number(x / 10);
+
+	_putchar(x % 10 + '0');
 }
