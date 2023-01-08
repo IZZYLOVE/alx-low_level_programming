a6e69e9be64419b840b717178e6cc1aa7e153203
IZZYLOVE <izzylovu@gmail.com>
2023-01-08 02:31:47 +0100

    on getting the concept

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -15,23 +15,28 @@ int main(int argc, char *argv[])
 	int num, j, result;
 	int coins[] = {25, 10, 5, 2, 1};
 
+	/* number of arguments passed to your program is not exactly 1 */
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
 
+	/* use atoi function to convert character numerics to intergers */
 	num = atoi(argv[1]);
 	result = 0;
 
+	/* if the number passed as the argument is negative, print 0 */
 	if (num < 0)
 	{
 		printf("0\n");
 		return (0);
 	}
 
+	/* using a for loop to select ememts of the coins array */
 	for (j = 0; j < 5 && num >= 0; j++)
 	{
+		/* keep subtracting value at coin[j] till num < coin[j] */
 		while (num >= coins[j])
 		{
 			result++;
