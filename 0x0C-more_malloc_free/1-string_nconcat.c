c3e0e5cdd83681eac7bd8094ecc022f5dbf8ffa5
IZZYLOVE <izzylovu@gmail.com>
2023-01-12 23:01:48 +0100

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -9,47 +9,34 @@
  *Return: pointer to new string
  */
 
-char *string_nconcat(char *s1, char *s2, unsigned int n)
+char *string_nconcat(char *s1, char *s2, unsigned int n)               
 {
 	char *s;
-	unsigned int i, len1 = 0, len2 = 0;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-	if (s1 == NULL)
-		s1 = '\0';
-	if (s1 == NULL)
-		s1 = '\0';
-
-
-
-	while (s1[len1] != '\0')
+	while (s1 && s1[len1])
 		len1++;
-	while (s2[len2] != '\0')
+	while (s2 && s2[len2])
 		len2++;
 
 	if (n < len2)
 		s = malloc(sizeof(char) * (len1 + n + 1));
-	else
-	{
+        else
 		s = malloc(sizeof(char) * (len1 + len2 + 1));
-		n = len2;
-	}
 
 	if (!s)
 		return (NULL);
 
-	if (s != NULL)
+	while (i < len1)
 	{
-		/* copy the first string to destination */
-		for (i = 0; i <= len1; i++)
-		{
-			s[i] = s1[i];
-		}
-
-		/* append the second string to destination */
-		for (i = 0; i <= n; i++)
-		{
-			s[len1 + i] = s2[i];
-		}
+		s[i] = s1[i];
+		i++;
 	}
+
+	while (n < len2 && i < (len1 + n))
+		s[i++] = s2[j++];
+	while (n >= len2 && i < (len1 + len2))
+		s[i++] = s2[j++];
+	s[i] = '\0';
 	return (s);
 }
