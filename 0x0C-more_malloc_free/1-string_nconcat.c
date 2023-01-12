6fff423cd30223d92209b9da5fa809de5d54b08c
IZZYLOVE <izzylovu@gmail.com>
2023-01-12 22:34:16 +0100

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,6 +14,13 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *s;
 	unsigned int i, len1 = 0, len2 = 0;
 
+	if (s1 == NULL)
+		s1 = '\0';
+	if (s1 == NULL)
+		s1 = '\0';
+
+
+
 	while (s1[len1] != '\0')
 		len1++;
 	while (s2[len2] != '\0')
