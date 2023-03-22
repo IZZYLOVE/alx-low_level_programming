ec13838aa4c14cc259369134165b1448f53b7020
IZZYLOVE <izzylovu@gmail.com>
2023-03-22 10:50:16 +0100

    101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.github.com/IZZYLOVE/alx-low_level_programming/master/0x18-dynamic_libraries/nrandom.so
-export LD_PRELOAD =/tmp/nrandom.so
+wget -P /tmp/ https://raw.github.com/IZZYLOVE/alx-low_level_programming/master/0x18-dynamic_libraries/0x18.c/libtest.so
+export LD_PRELOAD =/tmp/libtest.so
